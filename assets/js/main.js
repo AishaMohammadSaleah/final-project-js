3a3b5159c12a3a35b70b1134971d6449d50a0d23
AishaMohammadSaleh <147343243+AishaMohammadSaleah@users.noreply.github.com>
2025-03-16 22:13:55 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Carousel*/
+const myCarouselElement = document.querySelector('#carouselExampleFade')
+const myCarousel = new bootstrap.Carousel( myCarouselElement, {
+    interval: 4500, 
+    ride: 'carousel'  ,
+
+  });
